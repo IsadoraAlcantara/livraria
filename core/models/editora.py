1f0b397e8c09de476ae24f13ae83b07a5baae418
IsadoraAlcantara <isadoraalca405@gmail.com>
2024-03-12 09:19:13 -0300

    Criação da API para Editora

--- core/models/editora.py
+++ core/models/editora.py
@@ -3,6 +3,8 @@ from django.db import models
 class Editora(models.Model):
     nome = models.CharField(max_length=100)
     site = models.URLField(max_length=200, blank=True, null=True)
+    # entrada de dados blank=True não obrigatório
+    # banco null=True não obrigatório
 
     def __str__(self):
-        return self.nome
+        return f"{self.nome} ({self.id})"
