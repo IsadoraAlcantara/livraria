5477bd5cc17ee7f7db42e91be556bd699f0405fc
IsadoraAlcantara <isadoraalca405@gmail.com>
2024-03-19 09:25:31 -0300

    ajustes da api autor

--- core/views/autor.py
+++ core/views/autor.py
@@ -4,6 +4,6 @@ from core.models import Autor
 from core.serializers import AutorSerializer
 
 
-class EditoraViewSet(AutorViewSet):
+class AutorViewSet(ModelViewSet):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
