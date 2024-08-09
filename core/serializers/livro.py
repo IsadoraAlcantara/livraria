0938c6138b411bcf908c3fe1291673edb5639c6a
IsadoraAlcantara <isadoraalca405@gmail.com>
2024-08-09 08:37:52 -0300

    Dois serializer para livro

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -7,4 +7,9 @@ class LivroSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = "__all__"
-        depth = 1 # busca as fk, mostrando os atributos das fk no json
+
+class LivroDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+        depth = 1  # busca as fk, mostrando os atributos das fk no json
--- core/views/livro.py
+++ core/views/livro.py
@@ -1,10 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Livro
-from core.serializers import LivroSerializer
+from core.serializers import LivroSerializer, LivroDetailSerializer
 
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
     # queryset = Livro.objects.order_by("titulo") ordernando alfabeticamente pelo título
     serializer_class = LivroSerializer
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return LivroDetailSerializer
+        return LivroSerializer
