f7ab8ee8430986d41e8c42910f1449800c5d0fec
IsadoraAlcantara <isadoraalca405@gmail.com>
2024-08-13 09:12:31 -0300

    organização com black e isort

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -18,7 +18,6 @@ from core.views import (
     LivroViewSet,
     UserViewSet,
 )
-
 from uploader.router import router as uploader_router
 
 router = DefaultRouter()
--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -1,7 +1,6 @@
 from rest_framework.serializers import ModelSerializer, SlugRelatedField
 
 from core.models import Livro
-
 from uploader.models import Image
 from uploader.serializers import ImageSerializer
 
@@ -11,12 +10,13 @@ class LivroSerializer(ModelSerializer):
         model = Livro
         fields = "__all__"
         capa_attachment_key = SlugRelatedField(
-        source="capa",
-        queryset=Image.objects.all(),
-        slug_field="attachment_key",
-        required=False,
-        write_only=True,
-    )
+            source="capa",
+            queryset=Image.objects.all(),
+            slug_field="attachment_key",
+            required=False,
+            write_only=True,
+        )
+
     capa = ImageSerializer(required=False, read_only=True)
 
 
