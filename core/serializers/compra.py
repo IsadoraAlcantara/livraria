d57825edc0e57b342147dec0f0627a57260dc90e
IsadoraAlcantara <isadoraalca405@gmail.com>
2024-09-13 08:54:05 -0300

    Visualização dos itens da compra na listagem da compra

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,11 +1,17 @@
 from rest_framework.serializers import CharField, ModelSerializer
 
-from core.models import Compra
+from core.models import Compra, ItensCompra
+
+class ItensCompraSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = "__all__"
 
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source="usuario.email", read_only=True) # lê o email invés do id na api
     status = CharField(source="get_status_display", read_only=True) # muda o status da compra pra o texto invés do número
+    itens = ItensCompraSerializer(many=True, read_only=True)
 
     class Meta:
         model = Compra
-        fields = "(__all__)"
+        fields = "__all__"
