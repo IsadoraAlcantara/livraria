991113b8630aa8b9086795a05b1aa1f3087de3be
IsadoraAlcantara <isadoraalca405@gmail.com>
2024-09-13 08:31:40 -0300

    Endpoint para a listagem básica de compras

--- app/urls.py
+++ app/urls.py
@@ -14,6 +14,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from core.views import (
     AutorViewSet,
     CategoriaViewSet,
+    CompraViewSet,
     EditoraViewSet,
     LivroViewSet,
     UserViewSet,
@@ -22,10 +23,11 @@ from uploader.router import router as uploader_router
 
 router = DefaultRouter()
 
+router.register(r"autores", AutorViewSet, basename="autores")
 router.register(r"categorias", CategoriaViewSet, basename="categorias")
+router.register(r"compras", CompraViewSet, basename="compras")
 router.register(r"editoras", EditoraViewSet, basename="editoras")
 router.register(r"usuarios", UserViewSet, basename="usuarios")
-router.register(r"autores", AutorViewSet, basename="autores")
 router.register(r"livros", LivroViewSet, basename="livros")
 
 urlpatterns = [
